63d4e423cf58c95cee74248bb7603f85eba90ad1
James Narayanan <jamesnarayanan@gmail.com>
2025-10-13 04:33:16 +0100

    Fixed deploy path capitalization

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/pickomino-solver/",
+	base: "/Pickomino-Solver/",
 	plugins: [react()]
 });
